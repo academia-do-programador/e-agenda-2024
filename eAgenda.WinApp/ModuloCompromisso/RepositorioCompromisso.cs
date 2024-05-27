584fbd36c938628c956e7e97bbb0ace3b4fd9aa4
Tiago Santini <tiagosantiniathayde@outlook.com>
2024-05-27 15:09:36 -0300

    Implementando expressões lambdas

--- eAgenda.WinApp/ModuloCompromisso/RepositorioCompromisso.cs
+++ eAgenda.WinApp/ModuloCompromisso/RepositorioCompromisso.cs
@@ -4,43 +4,20 @@ namespace eAgenda.WinApp.ModuloCompromisso
 {
     public class RepositorioCompromisso : RepositorioBase<Compromisso>
     {
+        public List<Compromisso> SelecionarCompromissosPorPeriodo(DateTime dataInicio, DateTime dataTermino)
+        {
+            return registros
+                .FindAll(c => c.Data >= dataInicio && c.Data <= dataTermino);
+        }
+
         public List<Compromisso> SelecionarCompromissosFuturos()
         {
-            List<Compromisso> compromissosFuturos = new List<Compromisso>();
-
-            foreach (Compromisso compromisso in registros)
-            {
-                if (compromisso.Data >= DateTime.Today)
-                    compromissosFuturos.Add(compromisso);
-            }
-
-            return compromissosFuturos;
+            return registros.FindAll(c => c.Data >= DateTime.Today);
         }
 
         public List<Compromisso> SelecionarCompromissosPassados()
         {
-            List<Compromisso> compromissosPassados = new List<Compromisso>();
-
-            foreach (Compromisso compromisso in registros)
-            {
-                if (compromisso.Data < DateTime.Today)
-                    compromissosPassados.Add(compromisso);
-            }
-
-            return compromissosPassados;
-        }
-
-        public List<Compromisso> SelecionarCompromissosPorPeriodo(DateTime dataInicio, DateTime dataTermino)
-        {
-            List<Compromisso> compromissosDoPeriodo = new List<Compromisso>();
-
-            foreach (Compromisso c in registros)
-            {
-                if (c.Data >= dataInicio && c.Data <= dataTermino)
-                    compromissosDoPeriodo.Add(c);
-            }
-
-            return compromissosDoPeriodo;
+            return registros.FindAll(c => c.Data < DateTime.Today);
         }
     }
 }
